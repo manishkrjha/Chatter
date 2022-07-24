8b3ac07b6c48411ffda01e385c6460584cdd7dad
manishkrjha <manishkj2204@gmail.com>
2022-07-24 19:37:03 +0530

    added fetchChat api

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { protect } = require("../middleware/authMiddleware");
-const {accessChat} = require("../controllers/chatController");
+const {accessChat, fetchChats} = require("../controllers/chatController");
 
 
 router.route("/").post(protect, accessChat);
-// router.route("/").get(protect, fetchChats);
+router.route("/").get(protect, fetchChats);
 // router.route("/group").post(protect, createGroupChat);
 // router.route("/rename").put(protect, renameGroup);
 // router.route("/groupremove").put(protect, removeFromGroup);
