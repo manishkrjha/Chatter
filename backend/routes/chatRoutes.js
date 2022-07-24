45dc1f958ea3959e3e3f8d5b940f54510c780706
manishkrjha <manishkj2204@gmail.com>
2022-07-24 20:36:34 +0530

    adding chat endpoint

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const { protect } = require("../middleware/authMiddleware");
-const {accessChat, fetchChats, createGroupChat} = require("../controllers/chatController");
+const {accessChat, fetchChats, createGroupChat, renameGroup, addToGroup, removeFromGroup} = require("../controllers/chatController");
 
 
 router.route("/").post(protect, accessChat);
 router.route("/").get(protect, fetchChats);
 router.route("/group").post(protect, createGroupChat);
-// router.route("/rename").put(protect, renameGroup);
-// router.route("/groupremove").put(protect, removeFromGroup);
-// router.route("/groupadd").put(protect, addToGroup);
+router.route("/rename").put(protect, renameGroup);
+router.route("/groupadd").put(protect, addToGroup);
+router.route("/groupremove").put(protect, removeFromGroup);
 
 module.exports = router;
